0c39d64e002a7f98da07f1fd81a3cd8297a80dcf
Sarah Rehan <sarahahmad17@gmail.com>
2023-11-25 21:43:38 -0500

    setting redux

--- client/src/features/auth/authSlice.ts
+++ client/src/features/auth/authSlice.ts
@@ -1,4 +1,5 @@
 import {createSlice, createAsyncThunk} from '@reduxjs/toolkit'
+import authService from './authService'
 
 // Get user from local storage
 const user = JSON.parse(localStorage.getItem('user') as string)
@@ -11,6 +12,16 @@ const initialState = {
     message: ''
 }
 
+// Register user
+export const register = createAsyncThunk('auth/register', async (user, thunkAPI) => {
+    try {
+        return await authService.register(user)
+    } catch(err) {
+        const message = (err.response && err.response.data && err.response.data.message) || err.message || err.toString()
+        return thunkAPI.rejectWithValue(message)
+    }
+})
+
 export const authSlice = createSlice({
     name: 'auth',
     initialState,
@@ -22,7 +33,23 @@ export const authSlice = createSlice({
             state.message = ''
         }
     },
-    extraReducers: () => {}
+    extraReducers: (builder) => {
+        builder
+            .addCase(register.pending, (state) => {
+                state.isLoading = true
+            })
+            .addCase(register.fulfilled, (state, action) => {
+                state.isLoading = false
+                state.isSuccess = true
+                state.user = action.payload
+            })
+            .addCase(register.rejected, (state, action) => {
+                state.isLoading = false
+                state.isError = true
+                state.message = action.payload as string
+                state.user = null
+            })
+    }
 })
 
 export const { reset } = authSlice.actions
