216811f97a4748434835051d45a2993be9bbc48c
Sarah Rehan <sarahahmad17@gmail.com>
2023-11-28 13:39:12 -0500

    Updated profile form

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -1,23 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button, Container, Form } from 'react-bootstrap'
 import Title from '../components/Title'
 
 const Profile = () => {
+    
+    const initialState = {
+        name: "",
+        phone: "",
+        email: "",
+        location: "",
+        linkedIn: ""
+    }
+
+    const [formData, setFormData] = useState(initialState)
+
   return (
     <Container>
         <Title title="Profile Information" />
         <Form>
             <Form.Group className="mb-3" controlId="formName">
                 <Form.Label>Name</Form.Label>
-                <Form.Control type="text" placeholder="Name" />
+                <Form.Control type="text" />
             </Form.Group>
             <Form.Group className="mb-3" controlId="formPhone">
                 <Form.Label>Phone</Form.Label>
-                <Form.Control type="text" placeholder="Phone" />
+                <Form.Control type="text" />
             </Form.Group>
             <Form.Group className="mb-3" controlId="formEmail">
                 <Form.Label>Email</Form.Label>
-                <Form.Control type="text" placeholder="Email" />
+                <Form.Control type="text" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formLocation">
+                <Form.Label>Location</Form.Label>
+                <Form.Control type="text" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formLinkedIn">
+                <Form.Label>LinkedIn</Form.Label>
+                <Form.Control type="text" />
             </Form.Group>
             <Button variant="primary" type="submit">
                 Add
